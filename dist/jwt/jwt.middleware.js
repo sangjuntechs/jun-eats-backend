1f802c00f858eab5ea22b1713dfec0f7ebdaf8eb
sangjun <juntech0421@github.com>
2021-05-10 15:20:01 +0900

    jwtmiddleware error catch

--- dist/jwt/jwt.middleware.js
+++ dist/jwt/jwt.middleware.js
@@ -21,14 +21,14 @@ let jwtMiddleWare = class jwtMiddleWare {
     async use(req, res, next) {
         if ('x-jwt' in req.headers) {
             const token = req.headers['x-jwt'];
-            const decoded = this.jwtService.verify(token.toString());
-            if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-                try {
+            try {
+                const decoded = this.jwtService.verify(token.toString());
+                if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
                     const user = await this.userService.findById(decoded['id']);
                     req['user'] = user;
                 }
-                catch (error) { }
             }
+            catch (error) { }
         }
         next();
     }
