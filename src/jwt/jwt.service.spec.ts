a0cf8e907f0b0e93dfbfbdad2f5a8308a6a5e0d1
sangjun <juntech0421@github.com>
2021-05-24 17:35:05 +0900

    jwtService sign test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
 import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
 
 const TEST_KEY = 'testKey';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 describe('JWT Service', () => {
   let service: JwtService;
@@ -22,6 +29,13 @@ describe('JWT Service', () => {
   it('정의가 되는가', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sign');
+  describe('sign', () => {
+    it('사인된 토큰을 리턴하는가', () => {
+      const ID = 1;
+      service.sign(ID);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
   it.todo('verify');
 });
