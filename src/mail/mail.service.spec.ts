bd68e74b221ed71bb45fc98bf3a39c34c6b1ad6e
sangjun <juntech0421@github.com>
2021-05-25 17:07:33 +0900

    verificationEmail test

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -32,6 +32,19 @@ describe('MailService', () => {
   it('정의가 되는가', () => {
     expect(service).toBeDefined();
   });
-  it.todo('메일 보내기');
+  it('메일 보내기', () => {
+    const EMAIL = 'email';
+    const CODE = 'code';
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(service, 'sendEmail').mockImplementation(async () => {});
+    service.sendVerificationEmail(EMAIL, CODE);
+    expect(service.sendEmail).toHaveBeenCalledTimes(1);
+    expect(service.sendEmail).toHaveBeenLastCalledWith(
+      '이메일을 인증해주세요 - JunEats',
+      '이메일 인증 관련입니다.',
+      EMAIL,
+      CODE,
+    );
+  });
   it.todo('인증메일보내기');
 });
