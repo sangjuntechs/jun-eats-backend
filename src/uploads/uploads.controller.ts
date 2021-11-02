a7c3d0e2aa67e931125ddaa7724fdf290d1078a9
sangjun <juntech0421@github.com>
2021-11-02 19:18:13 +0900

    upload with aws image file

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,12 +5,36 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = 'sangjunjuneats123';
 
 @Controller('uploads')
 export class UploadsController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: 'AKIARHU5NBZ2W25BVAF6',
+        secretAccessKey: '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',
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const fileUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url: fileUrl };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
